f6562616ece132a72a55414b7d17afb1fc47316a
César.Desktop <paire.cesar@gmail.com>
2018-02-14 14:12:48 -0300

    testing

---
+++ ICESym-1D/src/newRun.py
@@ -0,0 +1,16 @@
+from numpy import array
+import time
+
+from simCythonCPP import Simulator
+
+#test = raw_input("Choise the Script with the data for the TEST (without '.py'): ")
+test='BoscaNew'
+data=__import__(test)
+now = time.time()
+
+Sim = Simulator(**data.kargs)
+print "termina de inicializar"
+Sim.printData()
+Sim.solver()
+now2 = time.time()
+print now2-now
